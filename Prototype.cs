6caefcac86ffa03d7bf85a1e54c15eace68e6b63
Irfat <imirfat10@gmail.com>
2023-07-18 22:36:29 +0600

    Prototype Design Pattern

---
+++ Prototype.cs
@@ -0,0 +1,26 @@
+﻿//Create complicated object in better way
+interface IPerson
+{
+    IPerson Clone();
+}
+class Person:IPerson
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public IPerson Clone()
+    {
+        return new Person();
+    }
+}
+
+public class PrototypeProgramme
+{
+    //public static void Main(string[] args)
+    //{
+    //    Person person1 = new Person();
+
+    //    //object creating by copying
+    //    Person person2 = person1.Clone() as Person;
+    //    Console.WriteLine(person2.FirstName);
+    //}
+}
