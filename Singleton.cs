c075ac1693f3c424036185a9c19b7d06d425072d
Irfat <imirfat10@gmail.com>
2023-07-18 15:26:36 +0600

    Add project files.

---
+++ Singleton.cs
@@ -0,0 +1,34 @@
+﻿//Singleton Design Pattern
+
+class SingleObject
+{
+    //creating the only object this class will have
+    private static SingleObject instance = new();
+
+    //making the constructor private
+    //so that no object can be created only the instance object can be used
+    private SingleObject() { }
+
+    //this method will return the only object
+    public static SingleObject getInstance()
+    {
+        return instance;
+    }
+
+    //this function can be used only with the instance object
+    public void Print()
+    {
+        Console.WriteLine("Hello world");
+    }
+}
+class Programme
+{
+    public static void Main(string[] args)
+    {
+        //get the object
+        SingleObject singleObject = SingleObject.getInstance();
+
+        //call the print function
+        singleObject.Print();
+    }
+}
